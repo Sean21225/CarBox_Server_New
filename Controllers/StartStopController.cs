aa8efe5e0e33a5e6f753ef356bd6ad2819f9ee2d
RonShlomo <109729895+RonShlomo@users.noreply.github.com>
2025-09-29 18:11:09 +0300

    Update StartStopController.cs

--- Controllers/StartStopController.cs
+++ Controllers/StartStopController.cs
@@ -101,7 +101,7 @@ namespace carbox.Controllers
     {
         public string CarId { get; set; }
         public string status { get; set; }
-        public int rideId { get; set; }
+        public string rideId { get; set; }
     }
 
     public class carboxCollection
